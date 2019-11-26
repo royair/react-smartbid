007cda5ed785dcb6c953bcd36f5d5e8296b300bd
roy yair <roy.yair@gmail.com>
2019-11-26 19:10:18 +0200

    Updates

---
+++ precache-manifest.c2d2a886d0ed6b1b588ac85909531a20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77f2a5a9bff9d33be495ca140c6d58c",
+    "url": "/react-smartbid/index.html"
+  },
+  {
+    "revision": "06fc86af9e7703e1159d",
+    "url": "/react-smartbid/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "fc8bf947bc3e1f981843",
+    "url": "/react-smartbid/static/css/main.d86f0d1f.chunk.css"
+  },
+  {
+    "revision": "06fc86af9e7703e1159d",
+    "url": "/react-smartbid/static/js/2.b5af7709.chunk.js"
+  },
+  {
+    "revision": "fc8bf947bc3e1f981843",
+    "url": "/react-smartbid/static/js/main.65d46266.chunk.js"
+  },
+  {
+    "revision": "46db6ebbfa288e67275f",
+    "url": "/react-smartbid/static/js/runtime-main.4c2afe2f.js"
+  },
+  {
+    "revision": "fc4fce5fac30327aa368fb74e45d72ef",
+    "url": "/react-smartbid/static/media/graph.fc4fce5f.png"
+  },
+  {
+    "revision": "cfe1ce661503aa293a686bf5cbcc1fec",
+    "url": "/react-smartbid/static/media/profile-image.cfe1ce66.jpg"
+  }
+]);
